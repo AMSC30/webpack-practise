188272df3c63ff4a64ec4136b6ad14f2a1884067
“zhengxixuan” <930042468@qq.com>
2021-08-12 20:11:02 +0800

    change-class

--- src/js/component.js
+++ src/js/component.js
@@ -3,7 +3,8 @@ import '../style/index.less'
 
 export function createHello() {
 	const div = document.createElement('div')
-	div.classList.add('hello')
+	div.classList.add('hello-css')
+	div.classList.add('hello-less')
 	div.innerHTML = ['hello', 'webpack'].join(' ')
 	return div
 }
