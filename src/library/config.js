1867f0f0882c58003414bcafa14e6425cef03659
zhengxixuan <930042468@qq.com>
2021-08-25 16:30:41 +0800

    修改打包库配置

--- src/library/config.js
+++ src/library/config.js
@@ -8,8 +8,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		filename: '[name].[contenthash:8].js',
-		library: 'math',
-		libraryTarget: 'umd'
+		library: { name: 'math', type: 'umd' }
 	},
 	optimization: {
 		usedExports: true,
